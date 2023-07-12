b71f1193d74914c5e6c9179570a104f905c9afc1
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-07-12 16:42:29 -0600

    Add productData file

---
+++ productData.js
@@ -0,0 +1,26 @@
+export const productData = {
+    productName: 'Fall Limited Edition Sneakers',
+    companyName: 'Sneaker Company',
+    description: `These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.`,
+    price: 250,
+    discount: 50,
+    images: [
+        {
+            image: 'image-product-1.jpg',
+            thumbnail: 'image-product-1-thumbnail.jpg'
+        },
+        {
+            image: 'image-product-2.jpg',
+            thumbnail: 'image-product-2-thumbnail.jpg'
+        },
+        {
+            image: 'image-product-3.jpg',
+            thumbnail: 'image-product-3-thumbnail.jpg'
+        },
+        {
+            imgae: 'image-product-4.jpg',
+            thumbnail: 'image-product-4-thumbnail.jpg'
+        }
+    ]
+
+}
