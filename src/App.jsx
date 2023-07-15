b8375686412a789596b6f3850a33841679b526a9
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-07-15 15:03:55 -0600

    Add Cart component with layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState} from 'react'
 import Header from './components/Header'
 import ProductPage from './components/ProductPage'
+import Cart from './components/Cart'
 import { productData } from '../productData'
 
 import './App.css'
 
 function App() {
   const [cartItems, setCartItems] = useState(0)
+  const [openCart, setOpenCart] = useState(true)
 
   function addItems(quantity) {
     setCartItems(prevCartItems => prevCartItems + quantity)
@@ -15,11 +17,17 @@ function App() {
   return (
     <>
       <Header 
-        key={cartItems}
         cartItems={cartItems}
       />
+      {openCart && 
+      <Cart
+        thumbnail={productData.images[0].thumbnail}
+        name={productData.productName}
+        price={productData.price}
+        discount={productData.discount}
+        cartItems={cartItems}
+      />}
       <ProductPage
-        key={productData.price}
         name={productData.productName}
         company={productData.companyName}
         description={productData.description}
